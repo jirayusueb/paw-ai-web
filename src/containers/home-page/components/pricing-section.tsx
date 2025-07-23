6a200d7cf366c1726c8bdae43451fec2288126b5
artid.jirayu <jirayu.sueb@gmail.com>
2025-07-23 23:15:45 +0700

    feat(home): add NVIDIA logo and update pricing section

--- src/containers/home-page/components/pricing-section.tsx
+++ src/containers/home-page/components/pricing-section.tsx
@@ -1,67 +1,69 @@
 import { Separator } from "@/components/ui/separator";
+import {
+  ArrowRight,
+  ChevronRight,
+  ChevronsLeftRight,
+  Globe,
+  Lock,
+} from "lucide-react";
+import NVIDIA_LOGO from "@/assets/media/svg/nvidia-logo.svg";
+import Image from "next/image";
 
-interface PricingCardProps {
-  title: string;
-  features: string[];
-  specs?: string[];
-  price?: string;
-}
-
-function PricingCard({ title, features, specs, price }: PricingCardProps) {
+function PricingCard() {
   return (
-    <div className="border rounded-lg p-6 flex flex-col gap-4">
-      <div className="font-semibold text-lg">{title}</div>
-      {price && (
-        <>
-          <Separator />
-          <div className="text-2xl font-bold">{price}</div>
-        </>
-      )}
-      {specs && specs.length > 0 && (
-        <>
-          <Separator />
-          <div className="flex flex-col gap-2">
-            {specs.map((spec, index) => (
-              <div key={index} className="text-sm text-muted-foreground">
-                {spec}
-              </div>
-            ))}
-          </div>
-        </>
-      )}
+    <div className="rounded-2xl p-6 flex flex-col gap-4 border">
+      <div className="flex items-center gap-2 justify-between">
+        <div className="flex items-center gap-2">
+          <Image src={NVIDIA_LOGO} alt="nvidia logo" />
+          <p className="font-semibold text-lg">Nvidia RTX 5090</p>
+        </div>
+        <div>
+          <ChevronRight size={16} />
+        </div>
+      </div>
       <Separator />
       <div className="flex flex-col gap-2">
-        {features.map((feature, index) => (
-          <div key={index} className="text-sm">
-            {feature}
+        <div className="flex justify-between items-center">
+          <div className="text-sm flex items-center gap-2">
+            <span className="font-light">VRAM</span>
           </div>
-        ))}
+          <div className="text-sm">80GB</div>
+        </div>
+        <div className="flex justify-between items-center">
+          <div className="text-sm flex items-center gap-2">
+            <span className="font-light">RAM</span>
+          </div>
+          <div className="text-sm">120GB</div>
+        </div>
+        <div className="flex justify-between items-center">
+          <div className="text-sm flex items-center gap-2">
+            <span className="font-light">GPU</span>
+          </div>
+          <div className="text-sm">16vCPUs</div>
+        </div>
+      </div>
+      <Separator />
+      <div className="flex flex-col gap-2">
+        <div className="flex justify-between items-center">
+          <div className="text-sm flex items-center gap-2">
+            <Lock className="size-4" />
+            <span className="font-light">Secure Cloud</span>
+          </div>
+          <div className="text-sm">$2.39/hr</div>
+        </div>
+        <div className="flex justify-between items-center">
+          <div className="text-sm flex items-center gap-2">
+            <Globe className="size-4" />
+            <span className="font-light">Community Cloud</span>
+          </div>
+          <div className="text-sm">$1.99/hr</div>
+        </div>
       </div>
     </div>
   );
 }
 
 export function PricingSection() {
-  const pricingPlans = [
-    {
-      title: "Nvidia RTX 5090",
-      specs: ["Nvidia RTX 5090", "Nvidia RTX 5090", "Nvidia RTX 5090"],
-      features: ["Nvidia RTX 5090", "Nvidia RTX 5090"],
-    },
-    {
-      title: "Nvidia RTX 5090",
-      features: ["Nvidia RTX 5090", "Nvidia RTX 5090", "Nvidia RTX 5090"],
-    },
-    {
-      title: "Nvidia RTX 5090",
-      features: ["Nvidia RTX 5090", "Nvidia RTX 5090", "Nvidia RTX 5090"],
-    },
-    {
-      title: "Nvidia RTX 5090",
-      features: ["Nvidia RTX 5090", "Nvidia RTX 5090", "Nvidia RTX 5090"],
-    },
-  ];
-
   return (
     <div className="container mx-auto flex flex-col items-center py-40">
       <div className="flex flex-col items-center gap-10 text-center">
@@ -73,9 +75,14 @@ export function PricingSection() {
         </div>
       </div>
       <div className="grid grid-cols-4 gap-4 w-full mt-20">
-        {pricingPlans.map((plan, index) => (
-          <PricingCard key={index} {...plan} />
-        ))}
+        <PricingCard />
+        <PricingCard />
+        <PricingCard />
+        <PricingCard />
+        <PricingCard />
+        <PricingCard />
+        <PricingCard />
+        <PricingCard />
       </div>
     </div>
   );
